530d2eba592ccfd278aa6774de9ef56a5df64791
garlicOS® <14845347+the-garlic-os@users.noreply.github.com>
2022-04-25 12:44:10 -0500

    Change comments; add WIN32_LEAN_AND_MEAN

--- inpout.hpp
+++ inpout.hpp
@@ -4,12 +4,12 @@
  * On Linux, these come bundled with the operating system in sys/io.h,
  * meanwhile support for these functions on Windows comes from the third-party
  * InpOut32.dll library.
- * The functions are wrapped in a class so on Windows the DLL can be freed when
- * the program exits.
+ * The functions are wrapped in a class so on Windows we can free the DLL when
+ * the program exits and normalize the interface across platforms.
  */ 
 
-#ifndef INPOUT_H_
-#define INPOUT_H_
+#ifndef INPOUT_HPP_
+#define INPOUT_HPP_
 
 #include <cstdint>  // uint16_t
 
@@ -21,9 +21,14 @@ class AbstractInpOut {
   virtual void outb(uint16_t data, uint16_t port) const = 0;
 };
 
-
-// Platform-specific class definition
+/**
+ * Windows
+ * - Get inb and outb from third-party InpOut32.dll.
+ * - Free InpOut32.dll when the program exits.
+ * - Reverse inb and outb's argument orders to match the Linux version.
+ */
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>  // __stdcall, HINSTANCE, LoadLibrary, FreeLibrary,
                       // GetProcAddress
 
@@ -66,14 +71,18 @@ class InpOut : public AbstractInpOut {
   }
 
   void outb(uint16_t data, uint16_t port) const override {
-    // InpOut32 expects the arguments in the opposite order
+    // Reverse this argument order
     m_outb(port, data);
   }
 };
 
 
+/**
+ * Linux
+ * - Get inb and outb from the built-in sys/io.h.
+ */
 #elif defined(__linux__) || defined(__unix__) || defined(__linux) || defined(__unix)
-#include <sys/io.h>
+#include <sys/io.h>  // inb, outb
 
 class InpOut : public AbstractInpOut {
  public:
@@ -93,4 +102,4 @@ class InpOut : public AbstractInpOut {
 #error "Unsupported platform"
 #endif  // Platform-specific class definition
 
-#endif  // INPOUT_H_
+#endif  // INPOUT_HPP_
