9156a6d165bf09a03628ff5d2fb891c413f9ba13
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-05-01 12:55:54 -0500

    Create info.cpp

---
+++ info.cpp
@@ -0,0 +1,38 @@
+/**
+ * Displays state information about the beep speaker for development purposes.
+ */
+
+#include <cstdint>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <bitset>
+#include "./inpout.hpp"  // InpOut
+#include "./constants.hpp"
+
+using namespace constants;
+
+void clear_screen() {
+	std::cout << "\033[2J\033[1;1H";
+}
+
+int main() {
+	InpOut inp_out;
+
+	// while (true) {
+		// clear_screen();
+		std::cout << std::bin;
+		std::cout << "Keyboard control register: 0b"
+		          << std::bitset<8>(inp_out.inb(reg::keyboard_control))
+		          << std::endl;
+		std::cout << "            PIT Channel 2: 0b"
+		          << std::bitset<8>(inp_out.inb(reg::channel2))
+		          << std::endl;
+		std::cout << "                 PIT Mode: 0b"
+		          << std::bitset<8>(inp_out.inb(reg::mode))
+		          << std::endl;
+		// std::this_thread::sleep_for(std::chrono::milliseconds(1000/60));
+	// }
+
+	return 0;
+}
