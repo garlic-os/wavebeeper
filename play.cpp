967ee77129eac2328656a066a67a88af5d48e8f6
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-04-30 20:43:25 -0500

    More doing Windows-only things only on Windows

--- play.cpp
+++ play.cpp
@@ -1,13 +1,14 @@
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-// Set stdin to binary mode on Windows
-#include <stdio.h>     // stdin
-#include <fcntl.h>     // _setmode, _O_BINARY
-#include <io.h>        // _fileno
+	// Set stdin to binary mode on Windows
+	#include <stdio.h>     // stdin
+	#include <fcntl.h>     // _setmode, _O_BINARY
+	#include <io.h>        // _fileno
 #endif
 
 #include <iostream>    // std::cin, std::istream
 #include <cmath>       // std::round
 #include <cstdint>     // uint8_t, uint16_t, uint32_t
+#include <cstring>     // strncmp
 #include "inpout.hpp"  // InpOut
 
 
@@ -18,148 +19,151 @@
  *       The input stream's cursor is at the start of a valid SQR file.
  */
 class SQRPlayer {
- private:
-  // https://wiki.osdev.org/Programmable_Interval_Timer#I.2FO_Ports
-  static constexpr uint32_t PIT_TICKRATE = 1193180;
-  static constexpr uint8_t PIT_CHANNEL_2 = 0x42;
-  static constexpr uint8_t PIT_CMDREG = 0x43;
+  private:
+	// https://wiki.osdev.org/Programmable_Interval_Timer#I.2FO_Ports
+	static constexpr uint32_t PIT_TICKRATE = 1193180;
+	static constexpr uint8_t PIT_CHANNEL_2 = 0x42;
+	static constexpr uint8_t PIT_CMDREG = 0x43;
 
-  // https://wiki.osdev.org/PC_Speaker#The_Raw_Hardware
-  // 13.7.1 https://www.intel.com/content/dam/www/public/us/en/documents/datasheets/7-series-chipset-pch-datasheet.pdf
-  static constexpr uint8_t KB_CONTROL_PORT = 0x61;
-
-
-  InpOut& m_inp_out = InpOut();  // An InpOut instance for accessing the beep
-                                 // speaker and PIT.
-  std::istream& m_input;  // The input stream to read from (e.g. cin or a file).
-  uint32_t m_sample_rate;  // Samples per second (i.e. Hz).
-  uint32_t m_data_size;  // The reported size of the SQR file's data section.
+	// https://wiki.osdev.org/PC_Speaker#The_Raw_Hardware
+	// 13.7.1 https://www.intel.com/content/dam/www/public/us/en/documents/datasheets/7-series-chipset-pch-datasheet.pdf
+	static constexpr uint8_t KB_CONTROL_PORT = 0x61;
 
 
-  // Move the beep speaker's diaphragm outward (excited state).
-  // https://wiki.osdev.org/PC_Speaker#The_Raw_Hardware
-  void beeper_out() const {
-    // Set bit 1 of the keyboard control register.
-    m_inp_out.outb(m_inp_out.inb(KB_CONTROL_PORT) | 0b00000010,
-                   KB_CONTROL_PORT);
-  }
-
-  // Move the beep speaker's diaphragm inward (relaxed state).
-  void beeper_in() const {
-    // Clear bit 1 of the keyboard control register.
-    m_inp_out.outb(m_inp_out.inb(KB_CONTROL_PORT) & 0b11111101,
-                   KB_CONTROL_PORT);
-  }
+	InpOut m_inp_out;  // An InpOut instance for accessing the beep speaker and PIT.
+	std::istream& m_input;  // The input stream to read from (e.g. cin or a file).
+	uint32_t m_sample_rate;  // Samples per second (i.e. Hz).
+	uint32_t m_data_size;  // The reported size of the SQR file's data section.
 
 
-  uint16_t freq2period(double freq) const {
-    return static_cast<uint16_t>(std::round(PIT_TICKRATE / freq));
-  }
+	// Move the beep speaker's diaphragm outward (excited state).
+	// https://wiki.osdev.org/PC_Speaker#The_Raw_Hardware
+	void beeper_out() const {
+		// Set bit 1 of the keyboard control register.
+		m_inp_out.outb(m_inp_out.inb(KB_CONTROL_PORT) | 0b00000010,
+									 KB_CONTROL_PORT);
+	}
 
-  // Set the frequency of Channel 2 of the PIT.
-  void set_pit_freq(double freq) const {
-    // Set Channel 2 to Square Wave mode.
-    // https://wiki.osdev.org/Programmable_Interval_Timer#Operating_Modes
-    m_inp_out.outb(PIT_CMDREG, 0b10110110);
-
-    // Set the channel's reload value to the period corresponding to the given
-    // frequency.
-    uint16_t period = freq2period(freq);
-    m_inp_out.outb(PIT_CHANNEL_2, static_cast<uint8_t>(period));
-    m_inp_out.outb(PIT_CHANNEL_2, static_cast<uint8_t>(period >> 8));
-  }
-
-  // Wait for Channel 2 of the PIT to complete its current cycle.
-  // If I could find a way to do this without eating a CPU core that'd be great
-  void pit_wait() const {
-    // The PIT alternates between some large value (not necessarily 0xFF) and
-    // some low value (not necessarily 0x00).
-    while (m_inp_out.inb(PIT_CHANNEL_2) < 127) {}
-  }
+	// Move the beep speaker's diaphragm inward (relaxed state).
+	void beeper_in() const {
+		// Clear bit 1 of the keyboard control register.
+		m_inp_out.outb(m_inp_out.inb(KB_CONTROL_PORT) & 0b11111101,
+									 KB_CONTROL_PORT);
+	}
 
 
-  // I forgot to write down what this does but the PIT doesn't cycle without it
-  void init_something() const {
-    // m_inp_out.outb(m_inp_out.inb(0x64) & 0b11101111, 0x64);
-    // m_inp_out.outb(0x60, 0x64);
-    // uint8_t guh_mode = m_inp_out.inb(0x60);
-    // m_inp_out.outb(0x60, 0x64);
-    // m_inp_out.outb(guh_mode | 0b00000001, 0x60);
-    m_inp_out.outb(m_inp_out.inb(KB_CONTROL_PORT) | 0b00000001,
-                   KB_CONTROL_PORT);
-  }
+	uint16_t freq2period(double freq) const {
+		return static_cast<uint16_t>(std::round(PIT_TICKRATE / freq));
+	}
 
-  void deinit_something() const {
-    // m_inp_out.outb(m_inp_out.inb(0x64) | 0b00010000, 0x64);
-    m_inp_out.outb(m_inp_out.inb(KB_CONTROL_PORT) & 0b00001110,
-                   KB_CONTROL_PORT);
-  }
+	// Set the frequency of Channel 2 of the PIT.
+	void set_pit_freq(double freq) const {
+		// Set Channel 2 to Square Wave mode.
+		// https://wiki.osdev.org/Programmable_Interval_Timer#Operating_Modes
+		m_inp_out.outb(PIT_CMDREG, 0b10110110);
 
+		// Set the channel's reload value to the period corresponding to the given
+		// frequency.
+		uint16_t period = freq2period(freq);
+		m_inp_out.outb(PIT_CHANNEL_2, static_cast<uint8_t>(period));
+		m_inp_out.outb(PIT_CHANNEL_2, static_cast<uint8_t>(period >> 8));
+	}
 
- public:
-  explicit SQRPlayer(std::istream& input_stream) : m_input(input_stream) {
-    // Verify that the input is a valid SQR file.
-    char buffer[4];
-    m_input.read(buffer, 4);
-    if (strncmp(buffer, "SQR\x00", 4) != 0) {
-      throw "Invalid SQR file";
-    }
-
-    // Read the metadata.
-    m_input.read(reinterpret_cast<char*>(&m_sample_rate), 4);
-    m_input.read(reinterpret_cast<char*>(&m_data_size), 4);
-  }
+	// Wait for Channel 2 of the PIT to complete its current cycle.
+	// If I could find a way to do this without eating a CPU core that'd be great
+	void pit_wait() const {
+		// The PIT alternates between some large value (not necessarily 0xFF) and
+		// some low value (not necessarily 0x00).
+		while (m_inp_out.inb(PIT_CHANNEL_2) < 0x7F) {}
+	}
 
 
-  virtual ~SQRPlayer() {
-    beeper_in();
-  }
+	// I forgot to write down what this does but the PIT doesn't cycle without it
+	void init_something() const {
+		// m_inp_out.outb(m_inp_out.inb(0x64) & 0b11101111, 0x64);
+		// m_inp_out.outb(0x60, 0x64);
+		// uint8_t guh_mode = m_inp_out.inb(0x60);
+		// m_inp_out.outb(0x60, 0x64);
+		// m_inp_out.outb(guh_mode | 0b00000001, 0x60);
+		m_inp_out.outb(
+			m_inp_out.inb(KB_CONTROL_PORT) | 0b00000001,
+			KB_CONTROL_PORT
+		);
+	}
+
+	void deinit_something() const {
+		// m_inp_out.outb(m_inp_out.inb(0x64) | 0b00010000, 0x64);
+		m_inp_out.outb(
+			m_inp_out.inb(KB_CONTROL_PORT) & 0b00001110,
+			KB_CONTROL_PORT
+		);
+	}
 
 
-  /**
-   * @post: The input stream's cursor is at the end of the SQR file's data
-   *        section or the EOF, whichever is encountered first.
-   */
-  void play() {
-    uint32_t bytes_read;
-    char sample_byte;
-    uint8_t bit_index;
+  public:
+	explicit SQRPlayer(std::istream& input_stream) : m_input(input_stream) {
+		// Verify that the input is a valid SQR file.
+		char buffer[4];
+		m_input.read(buffer, 4);
+		if (strncmp(buffer, "SQR\x00", 4) != 0) {
+			throw "Invalid SQR file";
+		}
 
-    // Set Channel 2 of the PIT to cycle at the given sample rate.
-    set_pit_freq(m_sample_rate);
-    // init_something();
+		// Read the metadata.
+		m_input.read(reinterpret_cast<char*>(&m_sample_rate), 4);
+		m_input.read(reinterpret_cast<char*>(&m_data_size), 4);
+	}
 
-    // Read samples in groups of 8 (1 byte) and play them.
-    for (bytes_read = 0;
-         bytes_read < m_data_size && m_input.get(sample_byte);
-         ++bytes_read) {
-      for (bit_index = 0; bit_index < 8; ++bit_index) {
-        if ((sample_byte >> (7 - bit_index)) & 1) {
-          // Bit is a 1, so move the speaker's diaphragm outward.
-          beeper_out();
-        } else {
-          // Bit is a 0, so move the speaker's diaphragm inward.
-          beeper_in();
-        }
-        pit_wait();  // Wait for the PIT to complete its current cycle,
-                     // which happens every sample-rate-th of a second.
-      }
-    }
 
-    beeper_in();
-    // deinit_something();
-  }
+	virtual ~SQRPlayer() {
+		beeper_in();
+	}
+
+
+	/**
+	 * @post: The input stream's cursor is at the end of the SQR file's data
+	 *        section or the EOF, whichever is encountered first.
+	 */
+	void play() {
+		uint32_t bytes_read;
+		char sample_byte;
+		uint8_t bit_index;
+
+		// Set Channel 2 of the PIT to cycle at the given sample rate.
+		set_pit_freq(m_sample_rate);
+		// init_something();
+
+		// Read samples in groups of 8 (1 byte) and play them.
+		for (bytes_read = 0;
+				 bytes_read < m_data_size && m_input.get(sample_byte);
+				 ++bytes_read) {
+			for (bit_index = 0; bit_index < 8; ++bit_index) {
+				if ((sample_byte >> (7 - bit_index)) & 1) {
+					// Bit is a 1, so move the speaker's diaphragm outward.
+					beeper_out();
+				} else {
+					// Bit is a 0, so move the speaker's diaphragm inward.
+					beeper_in();
+				}
+				pit_wait();  // Wait for the PIT to complete its current cycle,
+										 // which happens every sample-rate-th of a second.
+			}
+		}
+
+		beeper_in();
+		// deinit_something();
+	}
 };
 
 
 int main() {
-  #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
-  if (!_setmode(_fileno(stdin), _O_BINARY)) {
-    throw "Failed to set stdin to binary mode";
-  }
-  #endif
+	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+		if (!_setmode(_fileno(stdin), _O_BINARY)) {
+			throw "Failed to set stdin to binary mode";
+		}
+	#endif
 
-  SQRPlayer player(std::cin);
-  player.play();
-  return 0;
+	SQRPlayer player(std::cin);
+	player.play();
+	return 0;
 }
--- sqr2wav.cpp
+++ sqr2wav.cpp
@@ -1,7 +1,12 @@
-#include <stdio.h>
-#include <fcntl.h>
-#include <io.h>
-#include <iostream>
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+	#include <io.h>  // _setmode, _O_BINARY
+	#include <stdio.h>  // stdin, stdout, _fileno
+	// #include <fcntl.h>
+#endif
+
+
+#include <iostream>  // std::cin, std::cerr, std::cout, std::endl
+#include <cstring>  // strncmp
 
 #define INVALID_INPUT 1
 
@@ -12,56 +17,58 @@ using std::endl;
 
 
 int main() {
-  if (!_setmode(_fileno(stdin), _O_BINARY)) {
-    throw "Failed to set stdin to binary mode";
-  }
-  if (!_setmode(_fileno(stdout), _O_BINARY)) {
-    throw "Failed to set stdout to binary mode";
-  }
+	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
+		if (!_setmode(_fileno(stdin), _O_BINARY)) {
+			throw "Failed to set stdin to binary mode";
+		}
+		if (!_setmode(_fileno(stdout), _O_BINARY)) {
+			throw "Failed to set stdout to binary mode";
+		}
+	#endif
 
-  // Verify that the input is a valid SQR file.
-  {
-    char buffer[4];
-    cin.read(buffer, 4);
-    if (strncmp(buffer, "SQR\x00", 4) != 0) {
-      cerr << "Invalid SQR file" << endl;
-      exit(INVALID_INPUT);
-    }
-  }
+	// Verify that the input is a valid SQR file.
+	{
+		char buffer[4];
+		cin.read(buffer, 4);
+		if (strncmp(buffer, "SQR\x00", 4) != 0) {
+			cerr << "Invalid SQR file" << endl;
+			exit(INVALID_INPUT);
+		}
+	}
 
-  char sample_rate[4];
-  cin.read(sample_rate, 4);
+	char sample_rate[4];
+	cin.read(sample_rate, 4);
 
-  // Each 1-bit long sample from the input file is expanded out to 8-bits,
-  // making for 8 times the data size.
-  unsigned long data_size;
-  cin.read(reinterpret_cast<char*>(&data_size), 4);
-  data_size *= 8;
+	// Each 1-bit long sample from the input file is expanded out to 8-bits,
+	// making for 8 times the data size.
+	unsigned long data_size;
+	cin.read(reinterpret_cast<char*>(&data_size), 4);
+	data_size *= 8;
 
-  unsigned long file_size = data_size + 44;
+	unsigned long file_size = data_size + 44;
 
-  // Write the header for an 8-bit, mono WAV file with the same sample rate
-  // as the input file, according to this guide:
-  // https://docs.fileformat.com/audio/wav/
-  cout << "RIFF";
-  cout.write(reinterpret_cast<char*>(&file_size), 4);
-  cout << "WAVEfmt ";
-  cout.write("\x10\x00\x00\x00\x01\x00\x01\x00", 8);
-  cout.write(sample_rate, 4);
-  cout.write(sample_rate, 4);
-  cout.write("\x01\x00\x08\x00", 4);
-  cout << "data";
-  cout.write(reinterpret_cast<char*>(&data_size), 4);
+	// Write the header for an 8-bit, mono WAV file with the same sample rate
+	// as the input file, according to this guide:
+	// https://docs.fileformat.com/audio/wav/
+	cout << "RIFF";
+	cout.write(reinterpret_cast<char*>(&file_size), 4);
+	cout << "WAVEfmt ";
+	cout.write("\x10\x00\x00\x00\x01\x00\x01\x00", 8);
+	cout.write(sample_rate, 4);
+	cout.write(sample_rate, 4);
+	cout.write("\x01\x00\x08\x00", 4);
+	cout << "data";
+	cout.write(reinterpret_cast<char*>(&data_size), 4);
 
-  char sample;
-  while (cin.get(sample)) {
-    // Iterate over each bit in the file from left to right.
-    for (int i = 7; i >= 0; --i) {
-      // If the bit is set, write a 0xFF to the output file.
-      // Otherwise, write a 0x00.
-      cout.put((sample & (1 << i)) ? '\xFF' : '\x00');
-    }
-  }
+	char sample;
+	while (cin.get(sample)) {
+		// Iterate over each bit in the file from left to right.
+		for (int i = 7; i >= 0; --i) {
+			// If the bit is set, write a 0xFF to the output file.
+			// Otherwise, write a 0x00.
+			cout.put((sample & (1 << i)) ? '\xFF' : '\x00');
+		}
+	}
 
-  return 0;
+	return 0;
 }
