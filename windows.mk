33ae74a754cebe71d05aedd07f455d8a6cc37bbc
the-garlic-os <14845347+garlic-os@users.noreply.github.com>
2023-04-30 22:24:51 -0500

    Add debug stuff

--- windows.mk
+++ windows.mk
@@ -1,16 +1,22 @@
 SOURCES := $(wildcard *.cpp)
 OBJECTS := $(SOURCES:.cpp=.obj)
 EXECUTABLES := $(SOURCES:.cpp=.exe)
+ILKS := $(SOURCES:.cpp=.ilk)
+SYMBOLS := $(SOURCES:.cpp=.pdb)
 
 CC := cl
 CFLAGS := /EHsc /MD /O2 /nologo
+DEBUGFLAGS := /EHsc /MD /Zi /nologo
 
 all: $(EXECUTABLES)
+
+debug: CFLAGS = $(DEBUGFLAGS)
+debug: $(EXECUTABLES)
 
 %.exe: %.cpp
 	$(CC) $(CFLAGS) $<
 
-.PHONY: clean
+.PHONY: clean debug
 
 clean:
-	del $(OBJECTS) $(EXECUTABLES)
+	del /Q $(OBJECTS) $(EXECUTABLES) $(ILKS) $(SYMBOLS)
