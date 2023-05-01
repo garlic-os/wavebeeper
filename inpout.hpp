6b13e096f16d47c6aedbb94a5b3919c8f961a098
Nate Kean <14845347+garlic-os@users.noreply.github.com>
2023-05-01 12:22:16 -0500

    Fix incorrect argument type
    Oops! inb and outb's data length is 8 bits, not 16!

--- inpout.hpp
+++ inpout.hpp
@@ -8,10 +8,10 @@
  * the program exits and normalize the interface across platforms.
  */ 
 
-#ifndef INPOUT_HPP_
-#define INPOUT_HPP_
+#ifndef INPOUT_HPP
+#define INPOUT_HPP
 
-#include <cstdint>  // uint16_t
+#include <cstdint>  // uint8_t, uint16_t
 
 
 class AbstractInpOut {
@@ -40,71 +40,70 @@ class AbstractInpOut {
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>  // __stdcall, HINSTANCE, LoadLibrary, FreeLibrary,
-                      // GetProcAddress
+											// GetProcAddress
 
-// TODO: Link InpOut32 as a LIB instead of loading it as a DLL
 class InpOut : public AbstractInpOut {
  private:
-  // Function pointers to the driver methods we need
-  typedef uint16_t (__stdcall *fp_inb)(uint16_t);
-  typedef void     (__stdcall *fp_outb)(uint16_t, uint16_t);
-  fp_outb m_outb;
-  fp_inb m_inb;
+	// Function pointers to the driver methods we need
+	typedef uint8_t (__stdcall *fp_inb)(uint16_t);
+	typedef void    (__stdcall *fp_outb)(uint16_t, uint8_t);
+	fp_outb m_outb;
+	fp_inb m_inb;
 
-  // The DLL handle
-  HINSTANCE m_handle;
+	// The DLL handle
+	HINSTANCE m_handle;
 
 
  public:
-  InpOut() {
-    if (m_handle == nullptr) {
-      throw "Failed to load driver";
-    }
-
-    m_outb = (fp_outb) GetProcAddress(m_handle, "Out32");
-    m_inb = (fp_inb) GetProcAddress(m_handle, "Inp32");
-
-    // Check that the driver was opened successfully
-    if (!GetProcAddress(m_handle, "IsInpOutDriverOpen")()) {
-      FreeLibrary(m_handle);
-      throw "Failed to open driver";
-    }
-  }
-
-  virtual ~InpOut() {
-    FreeLibrary(m_handle);
-  }
-
-  uint16_t inb(uint16_t port) const override {
-    return m_inb(port);
-  }
-
-  void outb(uint16_t data, uint16_t port) const override {
-    // Reverse this argument order
-    m_outb(port, data);
-  }
+	InpOut() {
 		m_handle = LoadLibrary("inpoutx64.dll");
+		if (m_handle == nullptr) {
+			throw "Failed to load driver";
+		}
+
+		m_outb = (fp_outb) GetProcAddress(m_handle, "Out32");
+		m_inb = (fp_inb) GetProcAddress(m_handle, "Inp32");
+
+		// Check that the driver was opened successfully
+		if (!GetProcAddress(m_handle, "IsInpOutDriverOpen")()) {
+			FreeLibrary(m_handle);
+			throw "Failed to open driver";
+		}
+	}
+
+	virtual ~InpOut() {
+		FreeLibrary(m_handle);
+	}
+
+	uint8_t inb(uint16_t port) const override {
+		return m_inb(port);
+	}
+
+	void outb(uint8_t data, uint16_t port) const override {
+		// Reverse this argument order
+		m_outb(port, data);
+	}
 };
 
 
 /**
  * Linux
- * - Get inb and outb from the built-in sys/io.h.
+ * - Just get inb and outb from the built-in sys/io.h.
  */
 #elif defined(__linux__) || defined(__unix__) || defined(__linux) || defined(__unix)
 #include <sys/io.h>  // inb, outb
 
 class InpOut : public AbstractInpOut {
  public:
-  InpOut() {}
+	InpOut() {}
 
-  uint16_t inb(uint16_t port) const override {
-    return inb(port);
-  }
+	uint8_t inb(uint16_t port) const override {
+		return inb(port);
+	}
 
-  void outb(uint16_t data, uint16_t port) const override {
-    outb(data, port);
-  }
+	void outb(uint8_t data, uint16_t port) const override {
+		outb(data, port);
+	}
 };
 
 
@@ -112,4 +111,4 @@ class InpOut : public AbstractInpOut {
 #error "Unsupported platform"
 #endif  // Platform-specific class definition
 
-#endif  // INPOUT_HPP_
+#endif  // INPOUT_HPP
