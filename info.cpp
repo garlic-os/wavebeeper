9f8a699c5ab428ab74fc2d95d090ef9b91a3d93c
the-garlic-os <14845347+garlic-os@users.noreply.github.com>
2023-05-02 22:00:26 -0500

    Reformat info layout

--- info.cpp
+++ info.cpp
@@ -16,26 +16,37 @@ void clear_screen() {
 	std::cout << "\033[2J\033[1;1H";
 }
 
+bool test_bit(uint8_t bit, uint8_t byte) {
+	return (byte & (1 << bit)) != 0;
+}
+
 int main() {
 	InpOut inp_out;
 
+	uint16_t pit2_readout;
+	uint8_t nmi_readout;
+	char ugh[2] = {' ', ' '};
+
 	while (true) {
-		clear_screen();
-		std::cout << "    NMI control register: "
-		          << std::bitset<8>(inp_out.inb(0x61))
-				  << "\n";
-		std::cout << "           PIT Channel 0: "
-		          << std::bitset<8>(inp_out.inb(reg::channel2-2))
+		// Read PIT Channel 2 once for the low byte,
+		// then again for the high byte.
+		pit2_readout = inp_out.inb(reg::channel2);
+		pit2_readout |= inp_out.inb(reg::channel2) << 8;
+
+		nmi_readout = inp_out.inb(reg::nmi_control);
+
+		if (test_bit(flag::nmi::tmr2_out_sts, nmi_readout)){
+			ugh[1] = 'X';
+		} else {
+			ugh[1] = ' ';
+		}
+
+		std::cout << std::bitset<8>(nmi_readout)
+		          << "    "
+				  << std::bitset<16>(pit2_readout)
+				  << ugh
 		          << std::endl;
-		std::cout << "           PIT Channel 2: "
-		          << std::bitset<8>(inp_out.inb(reg::channel2))
-		          << std::endl;
-		std::cout << "   Timer Counter 0 [5:0]: "
-		          << std::bitset<8>(inp_out.inb(0x40))
-		          << std::endl;
-		std::cout << "   Timer Counter 2 [5:0]: "
-		          << std::bitset<8>(inp_out.inb(0x42))
-		          << std::endl;
+
 		std::this_thread::sleep_for(std::chrono::milliseconds(1000/60));
 	}
 
